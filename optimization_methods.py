fixed gradient descent and add fastest descent

optimization_methods.py
@@ -97,25 +97,24 @@ def gradient_descend_swift(func, params, eps, method, start=0, end=1):
     
     dot0 = np.array(params)
     steps = [dot0]
-    f_alpha = lambda alpha: func(*(dot0+alpha*gradient(func, eps, dot0.tolist())))
-    step = method(f_alpha, start, end, eps)
-    dot1 = dot0-step*gradient(func, eps, dot0.tolist())
     
-    while(np.linalg.norm(dot1-dot0)>eps):
+    while(np.linalg.norm(gradient(func, eps, dot0.tolist()))>eps):
         
-        dot0 = dot1
-
-        steps.append(dot0)
-        
-        f_alpha = lambda alpha: func(*(dot0+alpha*gradient(func, eps, dot0.tolist())))
-        step = method(f_alpha, start, end, eps)
+        f_alpha = lambda alpha: func(*(dot0-alpha*gradient(func, eps, dot0.tolist())))
+        if method == 'fibonacci':
+            step = fibonacci_method(f_alpha, start, end, eps)
+        else:
+            step = golden_ratio_method(f_alpha, start, end, eps)
+            
         dot1 = dot0 - step*gradient(func, eps, dot0.tolist())
+        dot0 = dot1
+        steps.append(dot0)
         qty_steps+=1
 
         print('number of iteration: {}, current point: {}, function value: {}'.format(qty_steps, 
                                                                                       dot1, func(*dot1)))
     
-    print("Precision: {}".format(np.linalg.norm(dot1-dot0)))
+    print("Precision: {}".format(np.linalg.norm(gradient(func, eps, dot1.tolist()))))
     
     return steps
 
@@ -152,17 +151,10 @@ def gradient_descent(func, params, eps):
     gradient descent method to minimize a given function
     """
     qty_steps=1
-    step=0.0015 # step can be chosen in range(0.0001, 0.7)
+    step=1
     
     dot0 = np.array(params)
     steps = [dot0]
-    f0 = func(*dot0)
-    while(func(*(dot0-step*gradient(func, eps, dot0.tolist())))<f0):
-        step*=2
-    while(func(*(dot0-step*gradient(func, eps, dot0.tolist())))>=f0):
-        step*=0.5
-        
-    dot1 = dot0-step*gradient(func, eps, dot0.tolist())
     
     while(np.linalg.norm(gradient(func, eps, dot0.tolist()))>eps):
         f0 = func(*dot0)
@@ -171,17 +163,15 @@ def gradient_descent(func, params, eps):
         while(func(*(dot0-step*gradient(func, eps, dot0.tolist())))>=f0):
             step*=0.5
         
-        #print("V0: ", dot0, dot1, gradient(func, eps, dot0.tolist()), np.linalg.norm(dot1-dot0))
-        dot0 = dot1
         dot1 = dot0-step*gradient(func, eps, dot0.tolist())
+        dot0 = dot1
         steps.append(dot0)
         qty_steps+=1
-        #print("V1: ", dot0, dot1, gradient(func, eps, dot0.tolist()), np.linalg.norm(dot1-dot0))
         
         print('number of iteration: {}, current point: {}, function value: {}'.format(qty_steps,
                                                                                       dot1, func(*dot1)))
     
-    print("Precision: {}".format(np.linalg.norm(dot1-dot0)))
+    print("Precision: {}".format(np.linalg.norm(gradient(func, eps, dot1.tolist()))))
     
     return steps
 